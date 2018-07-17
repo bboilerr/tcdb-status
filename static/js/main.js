f03bc5bc80cd52fa2d5a9d3aabe946d6d6fe686e
Brent Rager <Brent.Rager@genesys.com>
2018-07-17 10:47:49 -0400

    Added priority filtering and list by owner and test plan.

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,32 @@
 let params = new URLSearchParams(location.search.slice(1));
 
+function testSuiteContainsFilteredTestCases(testSuite) {
+    return testSuite.TestCases.reduce((value, testCase) => {
+        return !app.priority || value || testCase.Priority.Id <= app.priority;
+    }, false);
+}
+
+function testSuitesContainFilteredTestCases(testSuites) {
+    return testSuites.reduce((value, testSuite) => {
+        return value || testSuite.TestCases.reduce((value, testCase) => {
+            return !app.priority || value || testCase.Priority.Id <= app.priority;
+        }, false);
+    }, false);
+}
 
 const app = new Vue({
     el: '#app',
     data: {
-        testSuitesByOwners: [],
+        testPlansMap: new Map(),
+        testPlansByOwners: [],
         successDate: moment(params.get('date'), 'YYYY-MM-DD'),
-        priority: params.get('priority') && parseInt(params.get('priority'))
+        priority: (params.get('priority') && parseInt(params.get('priority'))) || 0
     },
     methods: {
         isExecutionPass: (testCaseExecution) => {
             return testCaseExecution.Result === 'Passed' && moment(testCaseExecution.ExecutionTime) > app.successDate;
         },
-        filterTestCasesByPriority: (testCases) => {
+        filterTestCases: (testCases) => {
             if (app.priority) {
                 return testCases.filter((testCase) => {
                     return testCase.Priority.Id <= app.priority;
@@ -21,6 +35,13 @@ const app = new Vue({
                 return testCases;
             }
         },
+        testSuiteContainsFilteredTestCases: testSuiteContainsFilteredTestCases,
+        testSuitesContainFilteredTestCases: (testSuites) => {
+            return testSuites.reduce((value, testSuite) => {
+                return value || testSuiteContainsFilteredTestCases(testSuite);
+            }, false);
+        },
+
         displayExecutionDate: (testCaseExecution) => {
             return moment(testCaseExecution.ExecutionTime).format('YYYY-MM-DD');
         }
@@ -28,18 +49,37 @@ const app = new Vue({
 });
 
 async function getTestSuitesByOwner() {
-    app.testSuitesByOwners = await $.get({
+    const testSuitesByOwners = await $.get({
         url: '/api/test-suites-by-owner',
         dataType: 'json'
     });
 
-    for (const testSuitesByOwner of app.testSuitesByOwners) {
+    const ownersMap = new Map();
+
+    for (const testSuitesByOwner of testSuitesByOwners) {
+        const testPlanMap = new Map();
+
         for (const testSuite of testSuitesByOwner[1]) {
+            const testPlans = testSuite.TestPlans.map(testPlan => {
+                if (!app.testPlansMap.has(testPlan.Id)) {
+                    app.testPlansMap.set(testPlan.Id, testPlan);
+                }
+
+                return testPlan.Id
+            }).join(',');
             testSuite.TestCases.sort((a, b) => {
                 return a.Priority.Id - b.Priority.Id;
             });
+            if (!testPlanMap.has(testPlans)) {
+                testPlanMap.set(testPlans, []);
+            }
+            testPlanMap.get(testPlans).push(testSuite);
         }
+
+        ownersMap.set(testSuitesByOwner[0], Array.from(testPlanMap));
     }
+
+    app.testPlansByOwners = Array.from(ownersMap);
 }
 
 getTestSuitesByOwner();
